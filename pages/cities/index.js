15dc35fca14080a63b3abb3ca9e836f95ade9a6f
Nadine Grantz <nadine.grantz@googlemail.com>
2024-01-21 17:51:09 +0100

    add Link from react

--- pages/cities/index.js
+++ pages/cities/index.js
@@ -1,10 +1,12 @@
 import { cities } from "@/lib/data";
+import Link from "next/link";
 
 export default function Cities() {
   return (
     <>
-      <h1>Welcome to the bigest Cities</h1>
+      <h1>Welcome to the biggest Cities</h1>
       <ul>
+        <Link href="/cities"> Go to cities</Link>
         {cities.map((city) => (
           <li key={city.id}>{city.name}</li>
         ))}
